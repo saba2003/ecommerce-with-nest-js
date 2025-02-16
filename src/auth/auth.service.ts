71e01887cf50536900b3d9b07e6c90947757da47
kiu_maghlakelidze.saba <maghlakelidze.saba@kiu.edu.ge>
2025-02-16 14:54:19 +0400

    added exception handling on email duplication

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2'
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable()
 export class AuthService {
@@ -11,17 +12,25 @@ export class AuthService {
         //generate password hash
         const hash = await argon.hash(dto.password);
         //save the new user in the db
-        const user = await this.prisma.user.create({
-            data: {
-                email: dto.email,
-                hash,
-            },
-        })
-
-        // Return user without hash
-        const { hash: _, ...userWithoutHash } = user;
-
-        return userWithoutHash;
+        try {
+            const user = await this.prisma.user.create({
+                data: {
+                    email: dto.email,
+                    hash,
+                },
+            })
+    
+            // Return user without hash
+            const { hash: _, ...userWithoutHash } = user;
+    
+            return userWithoutHash;
+        } catch (e) {
+            if (e instanceof PrismaClientKnownRequestError)
+                if(e.code === 'P2002')
+                    throw new ForbiddenException('Credentials taken')
+            throw e;
+        }
+        
     }
     logIn(){
         return { msg: 'user log in'};
