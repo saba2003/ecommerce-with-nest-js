786fe02c384287a5a54fc7eede54bc4e1b619ffc
kiu_maghlakelidze.saba <maghlakelidze.saba@kiu.edu.ge>
2025-02-16 19:33:06 +0400

    replaced hardcoded url with dotenv

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { AuthModule } from './auth/auth.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [AuthModule, BookmarkModule, PrismaModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    AuthModule, 
+    BookmarkModule, 
+    PrismaModule
+  ],
 })
 export class AppModule {}
